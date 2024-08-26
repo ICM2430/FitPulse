ca3e3ff2d410096909abed44c0e45dd96ff300fa
Alejandra Casas <141421692+al-calo@users.noreply.github.com>
2024-08-26 18:17:32 -0500

    Creación proyecto y mainActivity

---
+++ app/src/main/java/com/example/fitpulseproyecto/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.fitpulseproyecto
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.fitpulseproyecto.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        binding.bIniciarSesion.setOnClickListener {
+            val intent = Intent(baseContext, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
