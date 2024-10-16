75068f98de35904e8560900cb55b609d2b484259
Andres Centanaro <centanaro02@gmail.com>
2024-10-15 22:07:29 -0500

    permisos Manifest

--- app/src/main/java/com/example/fitpulseproyecto/EditarPerfilActivity.kt
+++ app/src/main/java/com/example/fitpulseproyecto/EditarPerfilActivity.kt
@@ -1,8 +1,10 @@
 package com.example.fitpulseproyecto
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -10,14 +12,21 @@ import com.example.fitpulseproyecto.databinding.ActivityEditarPerfilBinding
 
 class EditarPerfilActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEditarPerfilBinding
+    private lateinit var getContentCamera: ActivityResultLauncher<Uri>
+    private lateinit var requestCameraPermission: ActivityResultLauncher<String>
+    private lateinit var requestGalleryPermission: ActivityResultLauncher<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityEditarPerfilBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(binding.root)
-
         binding.atras.setOnClickListener {
             val intent = Intent(this, MiPerfilActivity::class.java)
         }
+        initActivityResultLaunchers()
+    }
+
+    private fun initActivityResultLaunchers() {
+        TODO("Not yet implemented")
     }
 }
